13cbb281696424d37207da552fb026d3179c38e2
Puneet Arora <puneetar@buffalo.edu>
2013-04-28 21:38:24 -0400

    comit

--- src/edu/buffalo/cse/sql/plan/IndexScanNode.java
+++ src/edu/buffalo/cse/sql/plan/IndexScanNode.java
@@ -8,15 +8,18 @@
  */
 package edu.buffalo.cse.sql.plan;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
 
 import edu.buffalo.cse.sql.Schema;
+import edu.buffalo.cse.sql.Sql;
 
 public class IndexScanNode extends PlanNode.Leaf {
   public final String table;
   public final Schema.Table schema;
   public ExprTree condition;
+  boolean flag_setSchemaVar=false;
   
   public ExprTree getCondition() {
 	return condition;
@@ -55,10 +58,34 @@ public IndexScanNode(String table, String rangeVariable, Schema.Table schema,Exp
 
   public List<Schema.Var> getSchemaVars()
   {
+	
+	if(!flag_setSchemaVar && Sql.flag_hmp_tables_col_used){
+			setSchemaVars();
+			flag_setSchemaVar=true;
+		}
+	  
     List<Schema.Var> vars = new ArrayList<Schema.Var>();
     for(Schema.Column c : schema){
       vars.add(c.name);
     }
     return vars;
   }
+  
+  public void setSchemaVars(){
+		List<String> ls_col_name=new ArrayList<String>();
+		
+		if(Sql.hmp_tables_col_used.get(table)!=null)		
+		ls_col_name.addAll(Sql.hmp_tables_col_used.get(table));
+		
+		if(Sql.hmp_tables_col_used.get("nothing")!=null)
+		ls_col_name.addAll(Sql.hmp_tables_col_used.get("nothing"));
+	
+		
+		Iterator<Schema.Column> it_schema=schema.iterator();
+		while(it_schema.hasNext()){
+			if(!ls_col_name.contains(it_schema.next().name.name)){
+				it_schema.remove();		
+			}
+		}
+	}
 }
